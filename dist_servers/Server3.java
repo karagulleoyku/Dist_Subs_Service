1206c8d4b7673ce6eee616a2f9e258574aeb7fbb
Oyku Karagulle <150177584+karagulleoyku@users.noreply.github.com>
2024-12-30 23:25:44 +0300

    Update Server3.java

--- dist_servers/Server3.java
+++ dist_servers/Server3.java
@@ -10,20 +10,20 @@ public class Server3 {
             System.out.println("Server3 is listening on port " + port);
 
             while (true) {
-                Socket clientSocket = serverSocket.accept(); // Yeni istemci bağlantısını kabul et
+                Socket clientSocket = serverSocket.accept(); 
                 System.out.println("New client connected to Server3");
 
-                // İstemciyle iletişim kur
+            
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 PrintWriter out = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()), true);
 
-                String clientMessage = in.readLine(); // İstemciden mesaj oku
+                String clientMessage = in.readLine(); 
                 System.out.println("Server3 received: " + clientMessage);
 
-                String response = "Processed by Server3: " + clientMessage; // Mesajı işlem yap
-                out.println(response); // İstemciye cevap gönder
+                String response = "Processed by Server3: " + clientMessage; 
+                out.println(response); 
 
-                clientSocket.close(); // Bağlantıyı kapat
+                clientSocket.close();
             }
         } catch (Exception e) {
             System.err.println("Error in Server3: " + e.getMessage());
