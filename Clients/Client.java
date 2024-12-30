62d0a5df53be15b2345277e5df5f879beb50a0fe
Oyku Karagulle <150177584+karagulleoyku@users.noreply.github.com>
2024-12-30 23:24:07 +0300

    Update Client.java

--- Clients/Client.java
+++ Clients/Client.java
@@ -8,24 +8,23 @@ public class Client {
     private static final int[] SERVER_PORTS = {5001, 5002, 5003};
     
     public Subscriber subscribe(String nameSurname) {
-        // Try each server until successful
+ 
         for (int port : SERVER_PORTS) {
             try (Socket socket = new Socket(SERVER_HOST, port)) {
-                // Create subscription request
+ 
                 Subscriber request = Subscriber.newBuilder()
                     .setStatus(Subscriber.Status.SUBS)
                     .setNameSurname(nameSurname)
                     .build();
                 
-                // Send request
+       
                 request.writeDelimitedTo(socket.getOutputStream());
                 
-                // Read response
                 Subscriber response = Subscriber.parseDelimitedFrom(socket.getInputStream());
                 return response;
             } catch (IOException e) {
                 System.out.println("Failed to connect to server on port " + port);
-                // Try next server
+          
             }
         }
         throw new RuntimeException("All servers are unavailable");
@@ -34,7 +33,7 @@ public class Client {
     public static void main(String[] args) {
         Client client = new Client();
         try {
-            Subscriber response = client.subscribe("Jane DOE");
+            Subscriber response = client.subscribe("Oyku Karag");
             System.out.println("Subscribed with ID: " + response.getId());
         } catch (Exception e) {
             e.printStackTrace();
