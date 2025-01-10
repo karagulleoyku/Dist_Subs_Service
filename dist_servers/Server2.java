49ccf45ed2a576c0d3e8a39e086bfabcb795adf1
Oyku Karagulle <150177584+karagulleoyku@users.noreply.github.com>
2025-01-10 22:22:28 +0300

    Update Server2.java

--- dist_servers/Server2.java
+++ dist_servers/Server2.java
@@ -1,31 +1,44 @@
+package dist_servers;
 import java.io.*;
 import java.net.*;
+import dist_servers.CapacityOuterClass.Capacity; // Capacity sınıfını içe aktar
 
 public class Server2 {
     public static void main(String[] args) {
-        int port = 5002; // Server2 dinleme portu
+        int port = 5002; 
+        int subscriberCount = 10;
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Server2 is listening on port " + port);
 
             while (true) {
-                Socket clientSocket = serverSocket.accept(); 
-                System.out.println("New client connected to Server2");
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("New client connected to Server1");
 
-               
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                PrintWriter out = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()), true);
+                OutputStream out = clientSocket.getOutputStream();
 
-                String clientMessage = in.readLine(); 
+                String clientMessage = in.readLine();
                 System.out.println("Server2 received: " + clientMessage);
 
-                String response = "Processed by Server2: " + clientMessage; 
-                out.println(response); 
+                if ("CPCTY".equals(clientMessage)) {
+                    Capacity capacity = Capacity.newBuilder()
+                        .setServerId(2) 
+                        .setServerStatus(subscriberCount) 
+                        .setTimestamp(System.currentTimeMillis() / 1000) 
+                        .build();
 
-                clientSocket.close(); 
+                    out.write(capacity.toByteArray());
+                    out.flush();
+                    System.out.println("Capacity sent: " + capacity);
+                } else {
+                    System.out.println("Unknown message received.");
+                }
+
+                clientSocket.close();
             }
         } catch (Exception e) {
-            System.err.println("Error in Server2: " + e.getMessage());
+            System.err.println("Error in Server1: " + e.getMessage());
         }
     }
 }
